d2d5a0976535cc21adaeefaaca132350c7a0fdbe
Andrew-Ogundimu <56798377+Andrew-Ogundimu@users.noreply.github.com>
2021-04-26 09:58:14 -0400

    Textures hooray :D
    added textures lesgo

--- src/Objects/MainWindow.java
+++ src/Objects/MainWindow.java
@@ -19,6 +19,7 @@ public class MainWindow implements GLEventListener {
     int[] vao;
     float r;
     boolean up;
+    Texture texture;
     VertexArray va;
     BufferLayout layout;
     VertexBuffer vb;
@@ -34,7 +35,7 @@ public class MainWindow implements GLEventListener {
         canvas = new GLCanvas(cap);
         canvas.addGLEventListener(this);
         JFrame window = new JFrame("Test");
-        window.setSize(640,360);
+        window.setSize(640,640);
         window.add(canvas);
         window.setVisible(true);
         window.setDefaultCloseOperation(window.EXIT_ON_CLOSE);
@@ -47,10 +48,10 @@ public class MainWindow implements GLEventListener {
         System.out.println(OpenGL.glGetString(GL3.GL_VERSION));
 
         positions = new float[]{
-                -0.5f, -0.5f,
-                0.5f, -0.5f,
-                0.5f, 0.5f,
-                -0.5f,0.5f};
+                -0.5f, -0.5f,0.0f,1.0f,
+                0.5f, -0.5f, 1.0f, 1.0f,
+                0.5f, 0.5f, 1.0f,0.0f,
+                -0.5f,0.5f,0.0f,0.0f};
 
         indices = new int[]{
                 0, 1, 2,
@@ -58,11 +59,12 @@ public class MainWindow implements GLEventListener {
         };
         //System.out.println(Arrays.toString(array));
         //create vertex buffer object (vbo)
-
+        //OpenGL.glBlendFunc()
         va = new VertexArray();
         vb = new VertexBuffer(positions);
 
         layout = new BufferLayout();
+        layout.push(GL3.GL_FLOAT,2);
         layout.push(GL3.GL_FLOAT,2);
         va.addBuffer(vb,layout);
 
@@ -82,6 +84,9 @@ public class MainWindow implements GLEventListener {
         int location = OpenGL.glGetUniformLocation(prog.id,"u_Color");
         //set uniform to float array
         OpenGL.glUniform4fv(location,1,new float[]{0.2f,0.3f,0.8f,1.0f},0);
+        Texture texture = new Texture("./src/Textures/alpha.png");
+        OpenGL.glUniform1i(OpenGL.glGetUniformLocation(prog.id,"u_Texture"),0);
+        texture.bind();
         r = 0.0f;
         up = false;
     }
--- src/render.java
+++ src/render.java
@@ -1,5 +1,14 @@
 import Objects.MainWindow;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Path;
+import java.util.Arrays;
+
 public class render {
     public static void main(String args[]) {
         MainWindow window = new MainWindow();
